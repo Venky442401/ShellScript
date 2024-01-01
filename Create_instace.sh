3957bc4488f7802e8cbd66925835db5af92cd3f3
venkat <venkatanavena442@gmail.com>
2024-01-01 16:03:41 +0530

    web setup

---
+++ Create_instace.sh
@@ -0,0 +1,1 @@
+aws ec2 run-instances --image-id <ami..> --count 1 --instance-type t2.micro --security-group-ids <secuity grupnma>
--- Web.sh
+++ Web.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+Validate(){
+    if[${1} -eq 0]
+}
+if [ $UID -ne 0 ]
+then 
+    echo "please run with root user"
+else
+    echo "root user"
+fi
+
 dnf install nginx -y
+if [$? -eq 0]
+then 
+    echo "Installed successfully"
+else
+    echo "nginx installation failed"
+fi
+
+#systemctl enable nginx
+#systemctl start nginx
