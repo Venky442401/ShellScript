86651b08d5c1030a81285494d09827351b8fdd5f
venkat <venkatanavena442@gmail.com>
2024-01-01 16:32:01 +0530

    web setup

--- Web.sh
+++ Web.sh
@@ -2,25 +2,23 @@
 Validate(){
     if[ $1 -ne 0 ]
     then 
-        echo "installation failed"
+        echo "$2 installation failed"
+        exit 1
     else
-        echo "Installed successfully"
+        echo "$2 Installed successfully"
     fi
 }
 if [ $UID -ne 0 ]
 then 
     echo "please run with root user"
+    exit 1
 else
     echo "root user"
 fi
 
 dnf install nginx -y
-if [ $? -ne 0 ]
-then 
-    echo "nginx installation failed"
-else
-    echo "Installed successfully"
-fi
-Validate $?,"nginx"
-#systemctl enable nginx
-#systemctl start nginx
+Validate $? "nginx"
+systemctl enable nginx
+Validate $? "nginx enable"
+systemctl start nginx
+Validate $? "nginx started"
