0f3554cab98dbc84931cfd0adad8325fc3a9be4d
venkat <venkatanavena442@gmail.com>
2024-01-01 17:12:42 +0530

    web setup

--- Web.sh
+++ Web.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
 Validate(){
     if [ $1 -ne 0 ]
     then 
@@ -22,3 +26,12 @@ systemctl enable nginx
 Validate $? "nginx enable"
 systemctl start nginx
 Validate $? "nginx started"
+rm -rf /usr/share/nginx/html/*
+Validate $? "remove defaullt web data"
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
+Validate $? "download web application"
+cd /usr/share/nginx/html
+Validate $? "change dirctory"
+unzip -o /tmp/web.zip
+Validate $? "unzip"
+#cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
